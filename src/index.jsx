969e91d38c97bad1bf81acdd64ca01e1dcf923a6
Leon Li <la2li@uwaterloo.ca>
2021-02-08 22:38:17 -0500

    add css

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,25 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import 'modern-css-reset/src/reset.css'
+import './index.css'
+import './fonts.css'
 
 const root = document.getElementById('root')
-ReactDOM.render(<div>Hello world!</div>, root)
+
+ReactDOM.render(
+  <>
+    <div style={{ padding: '2em' }}>
+      <div style={{ margin: '1em' }}>
+        <h1>Leon Li 👨🏻‍🔧</h1>
+      </div>
+      <div style={{ margin: '1em' }}>
+        <h2>
+          Full-stack developer
+          <br />
+          with 8+ years experience
+        </h2>
+      </div>
+    </div>
+  </>,
+  root
+)
