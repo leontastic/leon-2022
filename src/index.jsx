edcadcb5653d1b70e86b6a2aa99d04dad4a9227d
Leon Li <la2li@uwaterloo.ca>
2021-02-13 20:25:49 -0500

    split content into pages

--- src/index.jsx
+++ src/index.jsx
@@ -1,60 +1,13 @@
-import { Github, Linkedin } from '@icons-pack/react-simple-icons'
 import React from 'react'
 import ReactDOM from 'react-dom'
 import WebFont from 'webfontloader'
+import Home from './pages/Home'
 import 'modern-css-reset/src/reset.css'
 import './index.css'
-import TextFlipper from './components/TextFlipper'
-
-const disciplines = [
-  'Full-stack',
-  'JavaScript',
-  'TypeScript',
-  'Front-end',
-  'Back-end',
-  'React',
-  'Node.js',
-  'AWS',
-  'Cloud infrastructure',
-  'Test automation',
-]
 
 const render = () =>
   ReactDOM.render(
-    <>
-      <div style={{ padding: '1em 2em' }}>
-        <div style={{ margin: '1em' }}>
-          <h1>
-            <span style={{ whiteSpace: 'nowrap' }}>Leon Li</span> 👨🏻‍🔧
-          </h1>
-        </div>
-      </div>
-      <div style={{ padding: '1em 2em' }}>
-        <div style={{ margin: '1em' }}>
-          <h2>
-            <TextFlipper texts={disciplines} /> developer
-            <br />
-            with 8+ years of experience
-          </h2>
-        </div>
-      </div>
-      <div style={{ padding: '1em 2em' }}>
-        <div style={{ margin: '1em' }}>
-          <span style={{ fontSize: 24 }}>🚀</span>
-          <span style={{ margin: '0 1em', fontSize: 24 }}>Résumé</span>
-        </div>
-      </div>
-      <div style={{ padding: '1em 2em' }}>
-        <div style={{ margin: '1em', display: 'flex', alignItems: 'center' }}>
-          <Github color="#181717" size={24} />
-          <span style={{ margin: '0 1em', fontSize: 24 }}>Github</span>
-        </div>
-        <div style={{ margin: '1em', display: 'flex', alignItems: 'center' }}>
-          <Linkedin color="#0077B5" size={24} />
-          <span style={{ margin: '0 1em', fontSize: 24 }}>LinkedIn</span>
-        </div>
-      </div>
-    </>,
+    <Home />,
     document.getElementById('root')
   )
 
---
+++ src/pages/Home.jsx
@@ -0,0 +1,67 @@
+import { Github, Linkedin } from '@icons-pack/react-simple-icons'
+import React from 'react'
+import TextFlipper from '../components/TextFlipper'
+
+const disciplines = [
+  'Full-stack',
+  'JavaScript',
+  'TypeScript',
+  'Front-end',
+  'Back-end',
+  'React',
+  'Node.js',
+  'AWS',
+  'Cloud infrastructure',
+  'Test automation',
+]
+
+const Section = ({ children }) => (
+  <div style={{ padding: '1em 2em' }}>{children}</div>
+)
+
+const Block = ({ children, style = {}, ...props }) => (
+  <div style={{ margin: '1em', ...style }} {...props}>
+    {children}
+  </div>
+)
+
+const Home = () => {
+  return (
+    <>
+      <Section>
+        <Block>
+          <h1>
+            <span style={{ whiteSpace: 'nowrap' }}>Leon Li</span> 👨🏻‍🔧
+          </h1>
+        </Block>
+      </Section>
+      <Section>
+        <Block>
+          <h2>
+            <TextFlipper texts={disciplines} /> developer
+            <br />
+            with 8+ years of experience
+          </h2>
+        </Block>
+      </Section>
+      <Section>
+        <Block>
+          <span style={{ fontSize: 24 }}>🚀</span>
+          <span style={{ margin: '0 1em', fontSize: 24 }}>Résumé</span>
+        </Block>
+      </Section>
+      <Section>
+        <Block style={{ display: 'flex', alignItems: 'center' }}>
+          <Github color="#181717" size={24} />
+          <span style={{ margin: '0 1em', fontSize: 24 }}>Github</span>
+        </Block>
+        <Block style={{ display: 'flex', alignItems: 'center' }}>
+          <Linkedin color="#0077B5" size={24} />
+          <span style={{ margin: '0 1em', fontSize: 24 }}>LinkedIn</span>
+        </Block>
+      </Section>
+    </>
+  )
+}
+
+export default Home
